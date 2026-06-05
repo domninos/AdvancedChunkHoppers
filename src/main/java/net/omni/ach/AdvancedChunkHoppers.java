6c82c139d784edba89de8d76f8035ecad001515e
Aldrin Victoria <victoriaaldrin812@gmail.com>
2026-06-05 08:58:11 +0800

    Initial Commit

---
+++ src/main/java/net/omni/ach/AdvancedChunkHoppers.java
@@ -0,0 +1,16 @@
+package net.omni.ach;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AdvancedChunkHoppers extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
