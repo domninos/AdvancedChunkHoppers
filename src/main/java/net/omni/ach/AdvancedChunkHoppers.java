0a74de274dd525a76a5825a907a3e5be045bc9b8
Aldrin Victoria <victoriaaldrin812@gmail.com>
2026-06-16 23:16:43 +0800

    feat: minor changes

--- src/main/java/net/omni/ach/AdvancedChunkHoppers.java
+++ src/main/java/net/omni/ach/AdvancedChunkHoppers.java
@@ -3,11 +3,13 @@ package net.omni.ach;
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
 import net.omni.ach.db.DatabaseManager;
-import net.omni.ach.gui.GUIManager;
+import net.omni.ach.listeners.ChunkHopperListener;
+import net.omni.ach.listeners.GUIListener;
 import net.omni.ach.managers.CacheManager;
 import net.omni.ach.managers.ChunkHopperManager;
 import net.omni.ach.managers.FilterManager;
-import net.omni.ach.managers.StorageManager;
+import net.omni.ach.managers.GUIManager;
+import net.omni.ach.util.ConfigUtil;
 import net.omni.ach.util.MessageUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
@@ -21,45 +23,60 @@ public final class AdvancedChunkHoppers extends JavaPlugin {
         - use ItemPreStackEvent
         - use ItemSpawnEvent fallback
         -
-        - config
-        - add holograms support (picked up <item> x<amount>)
-        -
         - support PDC for player ownership
         - use SQLITE for inventories
         -
-        - implement auto save from pdc to database
+        - implement auto save to database
         - check for block changes / block destruction
+        -
+        - When the Chunk Hopper Gets full it should pull all the drops to above it and let them stack there and then once its empty should resume normal function
+        -
+        - When you break the hopper if it has items in it they should just pop out just like if you broke a chest.
+        -
+        - For whitelist and blacklist players should drag and drop an item into the inventory it makes an audible que and the item appears in the menu.
+        -
+        - The chunk hopper should act as some sort of over flow chest for when it gets full
+             and if it is full should message the player when a drop is unable to be picked up that way players know if that is the case.
      */
+
+    private boolean gangsEnabled, roseStackerEnabled = false;
 
     private FilterManager filterManager;
     private ChunkHopperManager chunkHopperManager;
 
     private CacheManager cacheManager;
-    private StorageManager storageManager;
     private GUIManager guiManager;
 
     private DatabaseManager databaseManager;
 
+    private ConfigUtil configUtil;
+
     @Override
     public void onDisable() {
+
+        // before flushing everything, save and close the pool
+        databaseManager.closePool();
 
         filterManager.flush();
         chunkHopperManager.flush();
 
         cacheManager.invalidateAll();
 
-        databaseManager.closePool();
+        configUtil.flush();
 
         sendConsole("<red>Successfully disabled.</red>");
     }
 
     @Override
     public void onEnable() {
+        saveDefaultConfig();
+
+        this.configUtil = new ConfigUtil(this);
+        configUtil.load();
 
         this.databaseManager = new DatabaseManager(this);
         databaseManager.initDatabase();
 
-        this.storageManager = new StorageManager(this);
         this.cacheManager = new CacheManager(this);
         this.guiManager = new GUIManager(this);
 
@@ -69,13 +86,41 @@ public final class AdvancedChunkHoppers extends JavaPlugin {
         this.chunkHopperManager = new ChunkHopperManager(this);
         chunkHopperManager.init();
 
-        // check if RoseStacker is enabled, if not, default to ChunkHopperListener
+        registerHooks();
+
+        registerCommands();
+
+        registerListeners();
 
         // getDescription() is deprecated on paper, should keep for spigot support
         sendConsole("<green>Successfully started <name>-v<version> </green>",
                 Placeholder.parsed("name", getDescription().getName()),
                 Placeholder.parsed("version", getDescription().getVersion())
         );
+    }
+
+    // TODO
+    private void registerHooks() {
+        // check if GangsPlus is enabled
+        if (Bukkit.getPluginManager().isPluginEnabled("GangsPlus")) {
+            gangsEnabled = true;
+            sendConsole("<green>GangsPlus is available!</green>");
+        }
+
+        // check if RoseStacker is enabled, if not, default to ChunkHopperListener
+        if (Bukkit.getPluginManager().isPluginEnabled("RoseStacker")) {
+            roseStackerEnabled = true;
+            sendConsole("<green>RoseStacker is available!</green>");
+        }
+    }
+
+    private void registerCommands() {
+
+    }
+
+    private void registerListeners() {
+        new ChunkHopperListener(this).register();
+        new GUIListener(this).register();
     }
 
     public void sendConsole(String message, TagResolver... resolvers) {
@@ -106,10 +151,6 @@ public final class AdvancedChunkHoppers extends JavaPlugin {
         return cacheManager;
     }
 
-    public StorageManager getStorageManager() {
-        return storageManager;
-    }
-
     public FilterManager getFilterManager() {
         return filterManager;
     }
@@ -118,12 +159,15 @@ public final class AdvancedChunkHoppers extends JavaPlugin {
         return chunkHopperManager;
     }
 
-
-    private void registerListeners() {
-
+    public ConfigUtil getConfigUtil() {
+        return configUtil;
     }
 
-    private void registerCommands() {
+    public boolean isGangsEnabled() {
+        return gangsEnabled;
+    }
 
+    public boolean isRoseStackerEnabled() {
+        return roseStackerEnabled;
     }
 }
