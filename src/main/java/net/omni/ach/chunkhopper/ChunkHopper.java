f442e5f975c26d1126745a39f5904624ed06a476
Aldrin Victoria <victoriaaldrin812@gmail.com>
2026-06-16 23:16:25 +0800

    feat: (WIP) ChunkHopper migration

---
+++ src/main/java/net/omni/ach/chunkhopper/ChunkHopper.java
@@ -0,0 +1,19 @@
+package net.omni.ach.chunkhopper;
+
+import org.bukkit.Location;
+import org.bukkit.inventory.Inventory;
+
+import java.util.UUID;
+
+public class ChunkHopper {
+    // TODO
+
+    private Inventory hopperInventory;
+    private Inventory whitelistInventory;
+    private Inventory blacklistInventory;
+
+    private Location location;
+    private UUID ownerUUID;
+
+
+}
