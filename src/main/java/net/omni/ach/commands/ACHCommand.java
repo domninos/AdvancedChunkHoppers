0f60ba3a3951602c75749f40c3dbd535476157b4
domninos <domninosdev@gmail.com>
2026-06-20 16:46:24 +0800

    feat: added /ach give

--- src/main/java/net/omni/ach/commands/ACHCommand.java
+++ src/main/java/net/omni/ach/commands/ACHCommand.java
@@ -3,10 +3,12 @@ package net.omni.ach.commands;
 import net.omni.ach.AdvancedChunkHoppers;
 import net.omni.ach.util.MessageUtil;
 import net.omni.ach.util.Messages;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.PluginCommand;
+import org.bukkit.entity.Player;
 import org.bukkit.util.StringUtil;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.jetbrains.annotations.NotNull;
@@ -53,11 +55,49 @@ public class ACHCommand implements CommandExecutor {
                 else
                     plugin.sendMessage(sender, Messages.RELOADED.toString());
             } else if (args[0].equalsIgnoreCase("about"))
-                plugin.getChatRenderer().sendMessage(sender, getAboutText());
+                plugin.sendMessage(sender, getAboutText());
+            else if (args[0].equalsIgnoreCase("give"))
+                plugin.sendMessage(sender, Messages.USAGE.replace("usage", "/ach give (player) [amount]"));
             else
                 plugin.sendMessage(sender, Messages.UNKNOWN_COMMAND.toString());
 
             return true;
+        } else if (args.length == 2 || args.length == 3) {
+            if (!args[0].equalsIgnoreCase("give")) {
+                plugin.sendMessage(sender, Messages.UNKNOWN_COMMAND.toString());
+                return true;
+            }
+
+            if (!(sender.hasPermission("ach.give"))) {
+                plugin.sendMessage(sender, Messages.NO_PERMS.toString());
+                return true;
+            }
+
+            Player target = Bukkit.getPlayerExact(args[1]);
+
+            if (target == null || !target.isOnline()) {
+                plugin.sendMessage(sender, Messages.PLAYER_NOT_FOUND.replace("player", args[1]));
+                return true;
+            }
+
+            int amount = 1;
+
+            if (args.length == 3) {
+                try {
+                    amount = Integer.parseInt(args[2]);
+                } catch (NumberFormatException e) {
+                    plugin.sendMessage(sender, "<red>'" + args[2] + "' is not a number. </red>");
+                    return true;
+                }
+            }
+
+            boolean success = plugin.getCustomCraftingHook().give(target, "customcrafting:utilities/chunk_hopper", amount);
+
+            if (success)
+                plugin.sendMessage(sender, Messages.GIVE_SUCCESS.replace("player", target.getName()));
+            else
+                plugin.sendMessage(sender, Messages.GIVE_ERROR.replace("player", target.getName()));
+
         } else
             plugin.sendMessage(sender, Messages.UNKNOWN_COMMAND.toString());
 
@@ -67,7 +107,7 @@ public class ACHCommand implements CommandExecutor {
     private void sendHelp(CommandSender sender) {
         StringBuilder helpBuilder = new StringBuilder();
 
-        helpBuilder.append("\n<dark_gray>▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪</dark_gray>\n");
+        helpBuilder.append("\n<dark_gray>▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪</dark_gray>\n");
         helpBuilder.append("  <gradient:#00AAFF:#55FFFF><bold>AdvancedChunkHoppers</bold></gradient> <gray>\n\n");
 
         if (sender.hasPermission("ach.use")) {
@@ -78,11 +118,14 @@ public class ACHCommand implements CommandExecutor {
                 helpBuilder.append(MessageUtil.formatString("ach <#55FFFF>reload</#55FFFF>", "Reloads config and messages."));
 
             helpBuilder.append(MessageUtil.formatString("ach <#55FFFF>about</#55FFFF>", "Shows basic information about this plugin."));
+
+            if (sender.hasPermission("ach.give"))
+                helpBuilder.append(MessageUtil.formatString("ach give <#55FFFF>(player) [amount]</#55FFFF>", "Gives chunk hopper to player."));
         }
 
-        helpBuilder.append("<dark_gray>▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪</dark_gray>");
+        helpBuilder.append("<dark_gray>▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪</dark_gray>");
 
-        plugin.getChatRenderer().sendMessage(sender, helpBuilder.toString());
+        sender.sendMessage(MessageUtil.parse(helpBuilder.toString()));
     }
 
     private @NonNull String getAboutText() {
@@ -112,17 +155,28 @@ public class ACHCommand implements CommandExecutor {
         }
 
         command.setTabCompleter((sender, command1, label, args) -> {
-            if (args.length == 1) {
-                List<String> subcommands = new ArrayList<>();
+            List<String> subcommands = new ArrayList<>();
+            List<String> completions = new ArrayList<>();
 
+            if (args.length == 1) {
                 subcommands.add("help");
                 subcommands.add("about");
 
                 if (sender.hasPermission("ach.reload"))
                     subcommands.add("reload");
 
-                List<String> completions = new ArrayList<>();
+                if (sender.hasPermission("ach.give"))
+                    subcommands.add("give");
+
                 StringUtil.copyPartialMatches(args[0], subcommands, completions);
+
+                return completions;
+            } else if (args.length == 2) {
+                if (args[0].equalsIgnoreCase("give") && sender.hasPermission("ach.give"))
+                    return null;
+            } else if (args.length == 3) {
+                if (args[0].equalsIgnoreCase("give") && sender.hasPermission("ach.give"))
+                    completions.add("[amount]");
 
                 return completions;
             }
